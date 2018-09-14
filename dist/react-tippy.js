d9b94b836a76accf53ad5d1478c69d28d3db9029
Davi Ferreira <contato@daviferreira.com>
2018-09-14 08:10:30 +0200

    Update build

--- dist/react-tippy.js
+++ dist/react-tippy.js
@@ -257,24 +257,27 @@ exports.default = closest;
 var _matches = __webpack_require__(8);
 
 /**
- * Ponyfill to get the closest parent element
- * @param {Element} element - child of parent to be returned
- * @param {String} parentSelector - selector to match the parent if found
- * @return {Element}
- */
-function closest(el, selector) {
-  if (Element.prototype.closest) {
-    return el.closest(selector);
+* Ponyfill to get the closest parent element
+* @param {Element} element - child of parent to be returned
+* @param {String} parentSelector - selector to match the parent if found
+* @return {Element}
+*/
+function closest(element, parentSelector) {
+  if (!element) {
+    return null;
   }
 
-  while (el) {
-    if (_matches.matches.call(el, selector)) {
-      return el;
+  var _closest = Element.prototype.closest || function (selector) {
+    var el = this;
+    while (el) {
+      if (_matches.matches.call(el, selector)) {
+        return el;
+      }
+      el = el.parentElement;
     }
-    el = el.parentElement;
-  }
+  };
 
-  return el;
+  return _closest.call(element, parentSelector);
 }
 
 /***/ }),
